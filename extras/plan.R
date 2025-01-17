b2e6f647413b70e33aec93c67359a82b0456ea5f
catalamarti <marticatalasabate@gmail.com>
2025-01-17 15:17:52 +0000

    Create plan.R

---
+++ extras/plan.R
@@ -0,0 +1,30 @@
+
+con
+cdmSchema = NULL
+writeSchema = NULL
+achillesSchema = NULL
+cohortTables = NULL
+cdmVersion = NULL
+cdmName = NULL
+logSql = NULL
+.softValidation = FALSE
+
+sparkFullName(schema, name)
+sparkListTables(con, schema)
+sparkReadTable(con, fullname)
+sparkWriteTable(con, fullname, table)
+sparkComputeTable(con, fullname, query, temporary)
+sparkDropTable(con, fullname) # try if we can make this work with temporary tables
+
+omopSparkSource(con, writeSchema) # class spark_cdm
+# con
+# writeSchema
+# temporary
+
+cdmTableFromSource.spark_cdm(src, value)
+compute.spark_cdm(x, name, temporary, overwrite) # source <- attr(x, "tbl_source")
+dropSourceTable.spark_cdm(src, name)
+insertTable.spark_cdm(src, name, table, overwrite, temporary)
+insertFromSource.spark_cdm(src, value) # remote_name
+listSourceTables.spark_cdm(src)
+readSourceTable.spark_cdm(src, name)
