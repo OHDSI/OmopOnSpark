f9eb4e3c400dffc232b687a9e8d207ef72bf93b2
cecicampanile <campanile.cecilia@gmail.com>
2025-01-23 12:25:48 +0000

    test deleted
    now it works only if catalog is in schema.
    must be tested.
    doesn't work with local connection at the moment

--- R/sparkDropTable.R
+++ R/sparkDropTable.R
@@ -3,9 +3,9 @@
 #' This function drops a specified table from a schema in a Spark connection, if it exists.
 #'
 #' @param con A valid Spark connection object. This is typically created using `sparklyr::spark_connect()`.
-#' @param schema A list specifying the schema details where the table is located. The list may contain:
+#' @param schema A list specifying the schema details where the table is located. The list must contain:
 #'   - `schema`: A string specifying the schema name.
-#'   - `catalog` (optional): If included, the function constructs a fully qualified table name with the catalog.
+#'   - `catalog`: A string specifying the catalog name.
 #' @param tableName A string specifying the name of the table to drop.
 #'
 #' @return The result of the `DROP TABLE` query execution as returned by `DBI::dbGetQuery()`.
@@ -22,18 +22,9 @@
 #' sparklyr::spark_disconnect(con)
 #' }
 #'
-#' @export
 sparkDropTable <- function(con, schema, tableName) {
+  full_name <- paste(schema$catalog, schema$schema, paste0(schema$prefix, tableName), sep = ".")
+  return(DBI::dbGetQuery(con, paste("DROP TABLE IF EXISTS", full_name)))
 
-  con <- validateConnection(con)
-  schema <- validateSchema(schema)
-  sparklyr::tbl_change_db(con, schema$schema)
-
-  if ("catalog" %in% names(schema)){
-    full_name <-  fullName(schema = schema, name = tableName)
-    return(DBI::dbGetQuery(con, paste("DROP TABLE IF EXISTS", full_name)))
-  }
-
-  return(DBI::dbGetQuery(con, paste("DROP TABLE IF EXISTS", tableName)))
 }
 
