ff2d0efabef05336260d9c62d99a69de0a6b77ee
edward-burn <edburn@hotmail.co.uk>
2025-03-14 13:49:33 +0000

    support tidyselect in drop table

--- R/method-dropSourceTable.R
+++ R/method-dropSourceTable.R
@@ -1,10 +1,35 @@
 #' @export
 dropSourceTable.spark_cdm <- function(cdm, name) {
+  namesCdm <- names(cdm)
+  namesSource <- listSourceTables(cdm = cdm)
+  toDrop <- c(namesCdm, namesSource) |>
+    unique() |>
+    selectTables(name = name)
+  toDropCdm <- namesCdm[namesCdm %in% toDrop]
+  toDropSource <- namesSource[namesSource %in% toDrop]
+
+  for(i in seq_along(toDropSource)){
   sparkDropTable(con = getCon(cdm),
                  schema = writeSchema(cdm),
                  prefix = writePrefix(cdm),
-                 name = name)
+                 name = toDropSource[i])
+  }
 }
+
+selectTables <- function(tables, name) {
+  res <- tables |>
+    rlang::set_names() |>
+    as.list() |>
+    dplyr::as_tibble() |>
+    dplyr::select(dplyr::any_of(name)) |>
+    colnames()
+  # drop settings, attrition and/or codelist tables
+  res <- res |>
+    purrr::map(\(x) paste0(x, c("", "_set", "_attrition", "_codelist"))) |>
+    purrr::flatten_chr()
+  unique(res[res %in% tables])
+}
+
 
 sparkDropTable <- function(con, schema, prefix, name) {
   if(is.null(prefix)){
--- tests/testthat/test-method-dropSourceTable.R
+++ tests/testthat/test-method-dropSourceTable.R
@@ -1,3 +1,45 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("dropping tables", {
+
+  folder <- file.path(tempdir(), "drop_spark")
+  cdm <- mockSparkCdm(path = folder)
+
+  cdm <- insertTable(cdm = cdm,
+              name = "cars",
+              table = cars,
+              temporary = FALSE)
+  expect_true("cars" %in% sparkListTables(con = getCon(cdm),
+                                  schema = writeSchema(cdm),
+                                  prefix = writePrefix(cdm)))
+  expect_true("cohort" %in% sparkListTables(con = getCon(cdm),
+                                          schema = writeSchema(cdm),
+                                          prefix = writePrefix(cdm)))
+  omopgenerics::dropSourceTable(cdm = cdm, name = "cars")
+  expect_true(!"cars" %in% sparkListTables(con = getCon(cdm),
+                                          schema = writeSchema(cdm),
+                                          prefix = writePrefix(cdm)))
+  expect_true("cohort" %in% sparkListTables(con = getCon(cdm),
+                                            schema = writeSchema(cdm),
+                                            prefix = writePrefix(cdm)))
+  # using tidyselect
+  cdm <- insertTable(cdm = cdm,
+                     name = "cars",
+                     table = cars,
+                     temporary = FALSE)
+  expect_true("cars" %in% sparkListTables(con = getCon(cdm),
+                                          schema = writeSchema(cdm),
+                                          prefix = writePrefix(cdm)))
+  expect_true("cohort" %in% sparkListTables(con = getCon(cdm),
+                                            schema = writeSchema(cdm),
+                                            prefix = writePrefix(cdm)))
+  omopgenerics::dropSourceTable(cdm = cdm, name = dplyr::starts_with("ca"))
+  expect_true(!"cars" %in% sparkListTables(con = getCon(cdm),
+                                           schema = writeSchema(cdm),
+                                           prefix = writePrefix(cdm)))
+  expect_true("cohort" %in% sparkListTables(con = getCon(cdm),
+                                            schema = writeSchema(cdm),
+                                            prefix = writePrefix(cdm)))
+
+  cdmDisconnect(cdm)
+  unlink(folder, recursive = TRUE)
+
 })
